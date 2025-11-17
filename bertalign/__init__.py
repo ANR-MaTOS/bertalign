b2bc3abc3c08cf4d44dde1b9dfc169b039ea9b2d
Ziqian Peng <zpeng@gpu014.localdomain>
2025-11-17 17:06:10 +0100

    debug & add gitignore

--- bertalign/__init__.py
+++ bertalign/__init__.py
@@ -10,15 +10,17 @@ from bertalign.encoder import Encoder
 # See other cross-lingual embedding models at
 # https://www.sbert.net/docs/pretrained_models.html
 
-# model_name = "LaBSE"
-model_name = "/gpfsdswork/dataset/HuggingFace_Models/sentence-transformers/LaBSE"
+model_name = "LaBSE"
+# model_name = "/gpfsdswork/dataset/HuggingFace_Models/sentence-transformers/LaBSE"
 model = Encoder(model_name)
 
-from bertalign.aligner import Bertalign
+# from bertalign.aligner import Bertalign
 
 
 # replace googletrans with fasttext, specify here the path to the fasttext model for language identification
 import fasttext
-lid_model = fasttext.load_model('/lustre/fswork/projects/rech/mrn/ujd84yr/FastText/lid.176.ftz')
-# lid_model = fasttext.load_model('/home/zpeng/scratch/MaTOS/resumeAllTHE/scripts/segment_align/lid_model/lid.176.ftz')
+# lid_model = fasttext.load_model('/lustre/fswork/projects/rech/mrn/ujd84yr/FastText/lid.176.ftz')
+lid_model = fasttext.load_model('/home/zpeng/scratch/MaTOS/resumeAllTHE/scripts/segment_align/lid_model/lid.176.ftz')
 
+
+from bertalign.aligner import Bertalign
